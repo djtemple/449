6ce70d411821755a966c963114d1c2e205291eba
Jeremy Kaiser <kaiserj95@gmail.com>
2017-02-03 08:43:20 -0700

    stuff

--- GameState.hs
+++ GameState.hs
@@ -1,29 +1,19 @@
 {- |
-Module      : ApocStrategyHuman
-Description : Template for a game-playing strategy definition.
-Copyright   : Copyright 2016, Rob Kremer (rkremer@ucalgary.ca), University of Calgary.
-License     : Permission to use, copy, modify, distribute and sell this software
-              and its documentation for any purpose is hereby granted without fee, provided
-              that the above copyright notice appear in all copies and that both that
-              copyright notice and this permission notice appear in supporting
-              documentation. The University of Calgary makes no representations about the
-              suitability of this software for any purpose. It is provided "as is" without
-              express or implied warranty.
-Maintainer  : rkremer@ucalgary.ca
-Stability   : experimental
-Portability : ghc 7.10.2 - 7.10.3
-
-This module is used for CPSC 449 for the Apocalypse assignment.
-
-This is merely a skeleton to get you started on creating a strategy for playing the
-Apocalypse game.  It has VERY little functionality.
+Module      : GameState
+Description : Functions that deal with the updating and maintainting of the  game
+state.
 -}
 
-module ApocStrategyHuman (
-   human
-   ) where
-
-import ApocTools
 
 {- | Game state code
 -}
+
+
+{- |
+Function to get locate and move given pieces to move based on given coordinates
+Parameters: (x,y) coordinates for human piece && (x',y') for updated move
+            (x,y) coordinates for computer piece && (x'y') for their move
+
+-}
+
+makeMove :: GameState
