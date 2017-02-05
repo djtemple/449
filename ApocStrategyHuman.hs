dc5ea3dcc59f78803898149c90762f8ed8503151
rarmclea <rarmclea@ucalgary.ca>
2017-02-05 12:35:30 -0700

    Update ApocStrategyHuman.hs
    I now have this working so that it will reprompt when necessary. The error checking set-up is there (checkMove, checkKnightMove, and checkPawnMove), but as I said on slack, that probably needs to be implemented in the gamestate update.

--- ApocStrategyHuman.hs
+++ ApocStrategyHuman.hs
@@ -23,45 +23,68 @@ module ApocStrategyHuman (
 
 import ApocTools
 
-{- | This is just a placeholder for the human strategy: it always chooses to play
-     (0,0) to (2,1).
+
+{- | Prompts the user for a move, checks input, and reprompts if input is invalid (out of range or insufficent length)
 -}
 human    :: Chooser
 human b Normal c = do
-<<<<<<< Updated upstream
   print (prompt Normal c)
-=======
-  if (c == Black)
-  then print "Enter the move coordinates for player Black in the form 'src x srcy dstx dst y'\n (0 <= n <= 4, or just enter return for a pass) B2: "
-  else print "Enter the move coordinates for player White in the form 'src x srcy dstx dst y'\n (0 <= n <= 4, or just enter return for a pass) W2: "
->>>>>>> Stashed changes
-  input <- getLine
-  let coords = getInts input
-  if (((length coords) == 0) || ((checkInput coords) == False))
-  then return Nothing
-  else return (Just [((coords !! 0), (coords !! 1)), ((coords !! 2), (coords !! 3))])
-
-{- | This is just a placeholder for the human strategy: it always chooses to play
-     (0,0) to (2,1).
--}
-human b PawnPlacement c = do
-<<<<<<< Updated upstream
-  print (prompt PawnPlacement c)
-=======
-  if (c == Black)
-  then print "B"
-  else print "W"
->>>>>>> Stashed changes
   input <- getLine
   let coords = getInts input
   if ((length coords) == 0)
   then return Nothing
-  else return (Just [((coords !! 0), (coords !! 1))])
+  else if (checkInput coords) 
+       then return (Just [((coords !! 0), (coords !! 1)), ((coords !! 2), (coords !! 3))])
+       else human b Normal c
+
+
+{- | TPrompts the user for a move, checks input, and reprompts if input is invalid (out of range or insufficent length)
+-}
+human b PawnPlacement c = do
+  print (prompt PawnPlacement c)
+  input <- getLine
+  let coords = getInts input
+  if ((length coords) == 0)
+  then return Nothing
+  else if (checkInput2 coords)
+       then return (Just [((coords !! 0), (coords !! 1))])
+       else human b PawnPlacement c
+
 
 {- | Helper function. Creates an integer list from the input string.
 -}
 getInts :: String -> [Int]
 getInts = map read . words
+
+
+{- | Helper function for normal moves. Checks the user input contains all necessary values and all values are in range
+-}
+checkInput :: [Int] -> Bool
+checkInput x | ((length x) < 4) = False
+             | (((x !! 0) < 0) || ((x !! 0) > 4)) = False
+             | (((x !! 1) < 0) || ((x !! 1) > 4)) = False
+             | (((x !! 2) < 0) || ((x !! 2) > 4)) = False
+             | (((x !! 3) < 0) || ((x !! 3) > 4)) = False
+             | otherwise = True
+
+
+{- | Helper function for pawn placement. Checks the user input contains all necessary values and all values are in range
+-}
+checkInput2 :: [Int] -> Bool
+checkInput2 x | ((length x) < 2) = False
+              | (((x !! 0) < 0) || ((x !! 0) > 4)) = False
+              | (((x !! 1) < 0) || ((x !! 1) > 4)) = False
+              | otherwise = True
+
+{- | Helper function. Selects the appropriate prompt based on player and playtype
+-}
+prompt :: PlayType -> Player -> String
+prompt Normal Black        = "Enter the move coordinates for player Black in the form 'srcx srcy dstx dsty'\n (0 <= n <= 4, or just enter return for a pass) B2: "
+prompt Normal White        = "Enter the move coordinates for player White in the form 'srcx srcy dstx dsty'\n (0 <= n <= 4, or just enter return for a pass) W2: "
+prompt PawnPlacement Black = "Enter the move coordinates for player Black in the form 'dstx dsty'\n (0 <= n <= 4, or just enter return for a pass) B1: "
+prompt PawnPlacement White = "Enter the move coordinates for player Black in the form 'dstx dsty'\n (0 <= n <= 4, or just enter return for a pass) W1: "
+
+
 
 {- | Checks that a players input move is valid
      Returns True if: the move follows the rules for the piece in the source square
@@ -80,6 +103,7 @@ checkMove b player src dst  | ((getFromBoard (theBoard b) src)== E)             
                             | (((getFromBoard (theBoard b) src) == BK) && (checkKnightMove b Black src dst)) = True
                             | (((getFromBoard (theBoard b) src) == BP) && (checkPawnMove b Black src dst))   = True
                             | otherwise                                                                      = False
+
 
 {- | Helper function. Checks that a pawn is moving appropriately - forward to an empty cell or diagonally forward to an enemy cell.
 -}
@@ -108,23 +132,3 @@ checkKnightMove b White (x,y) (x',y')| (getFromBoard (theBoard b) (x',y') == WP)
                                      | (((abs (x - x')) == 1) && ((abs (y - y')) == 2)) = True
                                      | (((abs (x - x')) == 2) && ((abs (y - y')) == 1)) = True
                                      | otherwise = False     
-
-
-{- | Helper function. Checks the user input contains all necessary values and all values are in range
--}
-checkInput :: [Int] -> Bool
-checkInput x | ((length x) < 4) = False
-             | (((x !! 0) < 0) || ((x !! 0) > 4)) = False
-             | (((x !! 1) < 0) || ((x !! 1) > 4)) = False
-             | (((x !! 2) < 0) || ((x !! 2) > 4)) = False
-             | (((x !! 3) < 0) || ((x !! 3) > 4)) = False
-             | otherwise = True
-
-{- | Helper function. Selects the appropriate prompt based on player and playtype
--}
-prompt :: PlayType -> Player -> String
-prompt Normal Black        = "Enter the move coordinates for player Black in the form 'srcx srcy dstx dsty'\n (0 <= n <= 4, or just enter return for a pass) B2: "
-prompt Normal White        = "Enter the move coordinates for player White in the form 'srcx srcy dstx dsty'\n (0 <= n <= 4, or just enter return for a pass) W2: "
-prompt PawnPlacement Black = "Enter the move coordinates for player Black in the form 'dstx dsty'\n (0 <= n <= 4, or just enter return for a pass) B1: "
-prompt PawnPlacement White = "Enter the move coordinates for player Black in the form 'dstx dsty'\n (0 <= n <= 4, or just enter return for a pass) W1: "
-
